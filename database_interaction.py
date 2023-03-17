c1e7290614652cc68e9fca727ac88dc255e55980
Sanya787 <114693100+Sanya787@users.noreply.github.com>
2023-03-17 16:15:55 +0300

    Changes related to the main code

--- database_interaction.py
+++ database_interaction.py
@@ -41,3 +41,33 @@ def get_from_base(id):
 WHERE id = {id}'''
     cursor.execute(query)
     return cursor.fetchall()[0][0]
+
+##################
+def append_to_statistics(id):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    query = f'''INSERT INTO statistics (id, victories, wins_now, total, friendly_victories) 
+    VALUES({id}, 0, 0, 0, 0);'''
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+
+def update_base_stat(id, data):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    query = f'''UPDATE statistics
+SET victories = "{data[0]}", wins_now = "{data[1]}", total = "{data[2]}", friendly_victories = "{data[3]}"
+WHERE id = {id};'''
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+
+def get_from_stat(id):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    query = f'''SELECT * FROM statistics
+WHERE id = {id}'''
+    cursor.execute(query)
+    return cursor.fetchall()[0]
