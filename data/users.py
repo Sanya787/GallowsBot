86df8cf7a5ab5bd45ecfe3cc11726c0e851cceab
Sanya787 <114693100+Sanya787@users.noreply.github.com>
2023-04-23 19:36:14 +0300

    The same code, but it passed the checks

--- data/users.py
+++ data/users.py
@@ -2,7 +2,7 @@ import sqlalchemy
 from data.db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase):  # type: ignore
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
